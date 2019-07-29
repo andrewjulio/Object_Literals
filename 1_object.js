c26a6aad43dff7eb00f3cbaad186a450bb4fba75
Andrew Julio <andrewjulio@hotmail.com>
2019-07-29 16:27:13 +0200

    Update 1_object.js

--- 1_object.js
+++ 1_object.js
@@ -8,15 +8,23 @@ If your program is stuck in an infinite loop, you can break out of the program b
 */
 
 // 1. Create an object named book using object literal syntax
-// 2. Add a title property to the book object and assign it a string value
-// 3. Add an author property to the book object and assign it a string value
-// 4. Add a publish_year to the book object and assign it a number to represent the year the book was published
+let book = {};
 
+// 2. Add a title property to the book object and assign it a string value
+book.title = "The Noise of Time";
+
+// 3. Add an author property to the book object and assign it a string value
+book.author = "Jules Barnes";
+
+// 4. Add a publish_year to the book object and assign it a number to represent the year the book was published
+book.publish_year = "2016";
 
 /* 5. Use a for-in loop to print the book object to the console so the final output looks something like this:
 title: Harry Potter and the Sorcerer's Stone
 author: J.K. Rowling
 publish_year: 1997
 */
-
+for(key in book) {
+    console.log(key + ": ", book[key]);
+}
 // 6. Run your code by typing node 1_object.js in the console below
